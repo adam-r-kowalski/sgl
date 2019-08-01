5fb0cc72b40ed70446af824301d5fc29a9c8f896
Adam Kowalski <adam.kowalski.work@gmail.com>
2019-07-31 20:52:49 -0700

    Get all tests passing again

--- include/sgl/core.hh
+++ include/sgl/core.hh
@@ -12,57 +12,59 @@ inline namespace v0 {
 
 // clang-format off
 template <class T, class U>
-concept Same = std::is_same_v<T, U> && std::is_same_v<U, T>;
+concept same_as = std::is_same_v<T, U> && std::is_same_v<U, T>;
 // clang-format on
 
-static constexpr size_t dynamic = -1;
+constexpr size_t dynamic = -1;
+template <size_t N> concept dynamic_v = N == dynamic;
 
-template <size_t N> concept Dynamic = N == dynamic;
-
-template <class T> constexpr size_t size_v = size(T{});
+template <class T> constexpr auto size_v = size(T{});
 
 template <class T> constexpr auto shape_v = shape(T{});
 template <class T> using shape_t = decltype(shape_v<T>);
 
 // clang-format off
 template <class D>
-concept Dimensions = requires() {
+concept dimensions = requires() {
   { size_v<D> } -> size_t;
   { shape_v<D> } -> shape_t<D>;
 };
 // clang-format on
 
-template <class T> constexpr size_t rank_v = rank(T{});
+template <class T> constexpr auto rank_v = rank(T{});
 
 template <class T>
-constexpr size_t dynamic_dimensions_v = dynamic_dimensions(T{});
+constexpr auto dynamic_dimensions_v = dynamic_dimensions(T{});
 
-template <size_t... Ns> struct dimensions {};
+template <size_t... Ns> struct dimension_list {};
 
-template <size_t... Ns> constexpr auto size(dimensions<Ns...>) -> size_t {
-  return (Dynamic<Ns> || ...) ? dynamic : (Ns * ...);
+template <size_t... Ns> constexpr auto size(dimension_list<Ns...>) -> size_t {
+  return (dynamic_v<Ns> || ...) ? dynamic : (Ns * ...);
 }
 
 template <size_t... Ns>
-constexpr auto shape(dimensions<Ns...>) -> std::array<size_t, sizeof...(Ns)> {
+constexpr auto shape(dimension_list<Ns...>)
+    -> std::array<size_t, sizeof...(Ns)> {
   return {Ns...};
 }
 
-constexpr auto rank(Dimensions auto d) -> size_t { return size(shape(d)); }
+constexpr auto rank(dimensions auto d) -> size_t { return size(shape(d)); }
 
 // TODO: no raw loops
-template <Dimensions D, class... Ds>
+template <dimensions D, class... Ds>
 auto runtime_shape(Ds... ds)
     -> std::array<size_t, rank_v<D>> requires(sizeof...(Ds) ==
                                               dynamic_dimensions_v<D>) {
   auto static_shape = shape_v<D>;
-  const auto dynamic_dimensions = std::array{ds...};
-  size_t j = 0;
-  for (size_t i = 0; i < size(static_shape); ++i)
-    if (static_shape[i] == dynamic) {
-      static_shape[i] = dynamic_dimensions[j];
-      ++j;
-    }
+  if constexpr (sizeof...(Ds) != 0) {
+    const auto dynamic_dimensions = std::array{ds...};
+    size_t j = 0;
+    for (size_t i = 0; i < size(static_shape); ++i)
+      if (static_shape[i] == dynamic) {
+        static_shape[i] = dynamic_dimensions[j];
+        ++j;
+      }
+  }
   return static_shape;
 }
 
@@ -72,81 +74,85 @@ template <class T> using value_t = typename value<T>::type;
 template <class T> struct reference { using type = value_t<T> &; };
 template <class T> using reference_t = typename reference<T>::type;
 
-template <class T> struct const_reference { using type = const value_t<T> &; };
+template <class T> struct const_reference { using type = value_t<T> const &; };
 template <class T> using const_reference_t = typename const_reference<T>::type;
 
 // clang-format off
 template <class I>
-concept Iterator =
-	requires(I i) {
-	  { *i } -> typename std::iterator_traits<I>::reference;
-		{ ++i } -> I&;
-	};
+concept iterator_type =
+  requires(I i) {
+    { *i } -> typename std::iterator_traits<I>::reference;
+    { ++i } -> I&;
+  };
 // clang-format on
 
-template <Iterator I> struct value<I> {
+template <iterator_type I> struct value<I> {
   using type = typename std::iterator_traits<I>::value_type;
 };
 
 // clang-format off
-template <class R> concept Range =
-	requires(R r) {
-		{ begin(r) } -> Iterator;
-		{ end(r) } -> Iterator;
-	};
+template <class R> concept range =
+  requires(R r) {
+    { begin(r) } -> iterator_type;
+    { end(r) } -> iterator_type;
+  };
 // clang-format on
 
-template <Range R> struct value<R> { using type = typename R::value_type; };
+template <range R> struct value<R> { using type = typename R::value_type; };
 
 // clang-format off
-template <class F, class Result, class... Args> concept Callable =
+template <class F, class Result, class... Args> concept callable =
   requires(F f, Args&&... args) {
-		{ f(std::forward<Args>(args)...) } -> Result;
-	};
+    { f(std::forward<Args>(args)...) } -> Result;
+  };
 // clang-format on
 
-template <Range R, class A, class E = const_reference_t<R>>
-constexpr auto reduce(R const &range, A accumulator, Callable<A, A, E> auto op)
+template <range R, class A, class E = const_reference_t<R>>
+constexpr auto reduce(R const &range, A accumulator, callable<A, A, E> auto op)
     -> A {
   for (auto const &element : range)
     accumulator = op(std::move(accumulator), element);
   return accumulator;
 }
 
-constexpr auto dynamic_dimensions(Dimensions auto d) -> size_t {
-  return reduce(shape(d), 0, [](const auto &a, const auto &v) {
+constexpr auto dynamic_dimensions(dimensions auto d) -> size_t {
+  return reduce(shape(d), 0, [](auto const &a, auto const &v) {
     return v == dynamic ? a + 1 : a;
   });
 }
 
 // clang-format off
-template <class S> concept Storage =
-  requires(S s, const S cs, size_t index) {
+template <class S> concept storage =
+  requires(S s, S const cs, size_t index) {
     { s[index] } -> reference_t<S>;
     { cs[index] } -> const_reference_t<S>;
   };
 // clang-format on
 
-template <class T, size_t N> struct storage {
+template <class T, size_t N> struct default_storage {
   using storage_type =
-      std::conditional_t<Dynamic<N>, std::vector<T>, std::array<T, N>>;
+      std::conditional_t<dynamic_v<N>, std::vector<T>, std::array<T, N>>;
 
-  explicit storage(size_t n) requires Dynamic<N> : data_(n) {}
-  storage() requires(!Dynamic<N>) = default;
+  explicit default_storage(size_t n) requires dynamic_v<N> : data_(n) {}
+  default_storage() requires(!dynamic_v<N>) = default;
 
   auto operator[](size_t index) -> T & { return data_[index]; }
-  auto operator[](size_t index) const -> const T & { return data_[index]; }
+  auto operator[](size_t index) const -> T const & { return data_[index]; }
 
 private:
   storage_type data_;
 };
 
-template <class T, size_t N> struct value<storage<T, N>> {
-  using type = value_t<typename storage<T, N>::storage_type>;
+template <class T, size_t N> struct value<default_storage<T, N>> {
+  using type = value_t<typename default_storage<T, N>::storage_type>;
 };
 
 // clang-format off
-template <class L> concept Layout = true;
+template <class L, class Shape = std::array<size_t, 3>> concept layout =
+  requires(L const cl, Shape shape, Shape cartesian_index) {
+    { L{shape} } -> L;
+    { cl.linear_index(cartesian_index) } -> size_t;
+  };
 // clang-format on
 
 template <size_t N> struct row_major {
@@ -183,41 +189,44 @@ private:
   std::array<size_t, N> stride_;
 };
 
-constexpr auto product(Range auto const &range) -> size_t {
-  return reduce(range, 1, std::multiplies{});
+template <range R, class T = value_t<R>>
+constexpr auto product(R const &range) -> T {
+  return reduce(range, T{1}, std::multiplies{});
 }
 
-template <class T, Dimensions D, Storage S = storage<T, size_v<D>>,
-          Layout L = row_major<rank_v<D>>>
+template <class T, dimensions D, storage S = default_storage<T, size_v<D>>,
+          layout L = row_major<rank_v<D>>>
 struct basic_tensor {
-  basic_tensor() requires(!Dynamic<size_v<D>>)
+  basic_tensor() requires(!dynamic_v<size_v<D>>)
       : shape_{shape_v<D>}, layout_{shape_} {}
 
   template <class... Ds>
-  explicit basic_tensor(Ds... ds) requires(Dynamic<size_v<D>>)
+  explicit basic_tensor(Ds... ds) requires(dynamic_v<size_v<D>>)
       : shape_{runtime_shape<D>(std::forward<Ds>(ds)...)},
         storage_{product(shape_)}, layout_{shape_} {}
 
   constexpr auto shape() const
-      -> std::array<size_t, rank_v<D>> requires(!Dynamic<size_v<D>>) {
+      -> std::array<size_t, rank_v<D>> requires(!dynamic_v<size_v<D>>) {
     return shape_v<D>;
   }
 
   auto shape() const
-      -> std::array<size_t, rank_v<D>> requires(Dynamic<size_v<D>>) {
-    return shape_;
+      -> std::array<size_t, rank_v<D>> requires(dynamic_v<size_v<D>>) {
+    return std::is_constant_evaluated() ? shape_v<D> : shape_;
   }
 
   template <class... Is>
-  friend auto index(basic_tensor &t, Is... cartesian_index)
+  friend auto index(basic_tensor &t, Is... is)
       -> T &requires(sizeof...(Is) == rank_v<D>) {
-    return t.storage_[t.layout_.linear_index(cartesian_index...)];
+    const auto cartesian_index = std::array<size_t, sizeof...(Is)>{is...};
+    return t.storage_[t.layout_.linear_index(cartesian_index)];
   }
 
   template <class... Is>
-  friend auto index(basic_tensor const &t, Is... cartesian_index)
+  friend auto index(basic_tensor const &t, Is... is)
       -> T const &requires(sizeof...(Is) == rank_v<D>) {
-    return t.storage_[t.layout_.linear_index(cartesian_index...)];
+    const auto cartesian_index = std::array<size_t, sizeof...(Is)>{is...};
+    return t.storage_[t.layout_.linear_index(cartesian_index)];
   }
 
 private:
@@ -226,14 +235,14 @@ private:
   L layout_;
 };
 
-template <class T, Dimensions D, Storage S, Layout L>
+template <class T, dimensions D, storage S, layout L>
 constexpr auto shape(basic_tensor<T, D, S, L> const &t)
     -> std::array<size_t, rank_v<D>> {
   return t.shape();
 }
 
 template <class T, size_t... Ns>
-using tensor = basic_tensor<T, dimensions<Ns...>>;
+using tensor = basic_tensor<T, dimension_list<Ns...>>;
 
 } // namespace v0
 
--- test/main.cc
+++ test/main.cc
@@ -7,58 +7,61 @@
 using namespace sgl;
 
 TEST_CASE("dimension model the Dimensions concept") {
-  static_assert(Dimensions<dimensions<3, 5, 7>>);
+  static_assert(dimensions<dimension_list<3, 5, 7>>);
 }
+
 
 TEST_CASE("dimension shape model the Range concept") {
-  static_assert(Range<decltype(shape_v<dimensions<3, 5, 7>>)>);
+  static_assert(range<decltype(shape_v<dimension_list<3, 5, 7>>)>);
 }
 
+
 TEST_CASE("dynamic dimensions have a dynamic size") {
-  static_assert(size_v<dimensions<dynamic, 5, 7>> == dynamic);
+  static_assert(size_v<dimension_list<dynamic, 5, 7>> == dynamic);
 }
 
 TEST_CASE("static dimension have a static shape") {
-  static_assert(shape_v<dimensions<3, 5, 7>> == std::array<size_t, 3>{3, 5, 7});
+  static_assert(shape_v<dimension_list<3, 5, 7>> == std::array<size_t, 3>{3, 5, 7});
 }
 
 TEST_CASE("dynamic dimension have a dynamic shape") {
-  static_assert(shape_v<dimensions<dynamic, 5, 7>> == std::array<size_t, 3>{dynamic, 5, 7});
+  static_assert(shape_v<dimension_list<dynamic, 5, 7>> == std::array<size_t, 3>{dynamic, 5, 7});
 }
 
 TEST_CASE("static dimension have a rank") {
-  static_assert(rank_v<dimensions<3, 5, 7>> == 3);
+  static_assert(rank_v<dimension_list<3, 5, 7>> == 3);
 }
 
 TEST_CASE("dynamic dimensions have a rank") {
-  static_assert(rank_v<dimensions<dynamic, 5, 7>> == 3);
+  static_assert(rank_v<dimension_list<dynamic, 5, 7>> == 3);
 }
 
 TEST_CASE("multiple dimensions can be dynamic") {
-  static_assert(rank_v<dimensions<dynamic, 5, dynamic>> == 3);
+  static_assert(rank_v<dimension_list<dynamic, 5, dynamic>> == 3);
 }
 
 TEST_CASE("dynamic_dimensions_v computes the number of dynamic dimensions") {
-  static_assert(dynamic_dimensions_v<dimensions<1, 2, 3>> == 0);
-  static_assert(dynamic_dimensions_v<dimensions<dynamic, 2, 3>> == 1);
-  static_assert(dynamic_dimensions_v<dimensions<dynamic, 2, dynamic>> == 2);
-  static_assert(dynamic_dimensions_v<dimensions<dynamic, dynamic, dynamic>> == 3);
+  static_assert(dynamic_dimensions_v<dimension_list<1, 2, 3>> == 0);
+  static_assert(dynamic_dimensions_v<dimension_list<dynamic, 2, 3>> == 1);
+  static_assert(dynamic_dimensions_v<dimension_list<dynamic, 2, dynamic>> == 2);
+  static_assert(dynamic_dimensions_v<dimension_list<dynamic, dynamic, dynamic>> == 3);
 }
 
+
 TEST_CASE("static dimension have a compile time size") {
-  static_assert(size_v<dimensions<3, 5, 7>> == 3 * 5 * 7);
+  static_assert(size_v<dimension_list<3, 5, 7>> == 3 * 5 * 7);
 }
 
 TEST_CASE("storage with known dimensions is array") {
-  static_assert(std::is_same_v<storage<int, 3>::storage_type, std::array<int, 3>>);
+  static_assert(same_as<default_storage<size_t, 3>::storage_type, std::array<size_t, 3>>);
 }
 
 TEST_CASE("storage with unknown dimensions is vector") {
-  static_assert(std::is_same_v<storage<int, dynamic>::storage_type, std::vector<int>>);
+  static_assert(same_as<default_storage<int, dynamic>::storage_type, std::vector<int>>);
 }
 
 TEST_CASE("storage with dynamic size can be indexed") {
-  auto s = storage<int, dynamic>{5};
+  auto s = default_storage<int, dynamic>{5};
 
   REQUIRE(s[0] == 0);
   REQUIRE(s[1] == 0);
@@ -80,7 +83,7 @@ TEST_CASE("storage with dynamic size can be indexed") {
 }
 
 TEST_CASE("storage with static size can be indexed") {
-  auto s = storage<int, 5>{};
+  auto s = default_storage<int, 5>{};
 
   REQUIRE(s[0] == 0);
   REQUIRE(s[1] == 0);
@@ -102,18 +105,18 @@ TEST_CASE("storage with static size can be indexed") {
 }
 
 TEST_CASE("storage has a value type") {
-	static_assert(Same<value_t<storage<int, 3>>, int>);
-	static_assert(Same<value_t<storage<int, dynamic>>, int>);
+  static_assert(same_as<value_t<default_storage<int, 3>>, int>);
+  static_assert(same_as<value_t<default_storage<int, dynamic>>, int>);
 }
 
 TEST_CASE("storage has a reference type") {
-	static_assert(Same<reference_t<storage<int, 3>>, int&>);
-	static_assert(Same<reference_t<storage<int, dynamic>>, int&>);
+	static_assert(same_as<reference_t<default_storage<int, 3>>, int&>);
+	static_assert(same_as<reference_t<default_storage<int, dynamic>>, int&>);
 }
 
 TEST_CASE("storage has a const reference type") {
-	static_assert(Same<const_reference_t<storage<int, 3>>, const int&>);
-	static_assert(Same<const_reference_t<storage<int, dynamic>>, const int&>);
+	static_assert(same_as<const_reference_t<default_storage<int, 3>>, const int&>);
+	static_assert(same_as<const_reference_t<default_storage<int, dynamic>>, const int&>);
 }
 
 
@@ -178,6 +181,13 @@ TEST_CASE("tensors can be default constructed if they have known dimensions") {
   static_assert(shape(t) == std::array<size_t, 3>{3, 5, 7});
 }
 
+TEST_CASE("runtime shape fills in missing dimensions at runtime") {
+  REQUIRE(runtime_shape<dimension_list<3, 5, 7>>() == std::array<size_t, 3>{3, 5, 7});
+  REQUIRE(runtime_shape<dimension_list<dynamic, 5, 7>>(3) == std::array<size_t, 3>{3, 5, 7});
+  REQUIRE(runtime_shape<dimension_list<dynamic, 5, dynamic>>(3, 7) == std::array<size_t, 3>{3, 5, 7});
+  REQUIRE(runtime_shape<dimension_list<dynamic, dynamic, dynamic>>(3, 5, 7) == std::array<size_t, 3>{3, 5, 7});
+}
+
 TEST_CASE("tensors can be constructed by providing dynamic dimensions") {
   const auto t1 = tensor<int, dynamic, 3, 4>{2};
   const auto t2 = tensor<int, 2, dynamic, 4>{3};
@@ -195,26 +205,32 @@ TEST_CASE("tensors can be constructed by providing dynamic dimensions") {
   REQUIRE(shape(t7) == std::array<size_t, 3>{2, 3, 4});
 }
 
+TEST_CASE("tensors can be indexed") {
+  auto t = tensor<int, 1, 2, 3>{};
+  //REQUIRE(t(0, 0, 0) == 0);
+}
+
 TEST_CASE("ranges have a value type") {
-	static_assert(Same<value_t<std::array<int, 3>>, int>);
+  static_assert(same_as<value_t<std::array<int, 3>>, int>);
 }
 
 TEST_CASE("ranges have a reference type") {
-	static_assert(Same<reference_t<std::array<int, 3>>, int&>);
+  static_assert(same_as<reference_t<std::array<int, 3>>, int&>);
 }
 
 TEST_CASE("ranges have a const reference type") {
-	static_assert(Same<const_reference_t<std::array<int, 3>>, const int&>);
+  static_assert(same_as<const_reference_t<std::array<int, 3>>, const int&>);
 }
 
 TEST_CASE("iterators have a value type") {
-	static_assert(Same<value_t<std::array<int, 3>::iterator>, int>);
+  static_assert(same_as<value_t<std::array<int, 3>::iterator>, int>);
 }
 
 TEST_CASE("iterators have a reference type") {
-	static_assert(Same<reference_t<std::array<int, 3>::iterator>, int&>);
+  static_assert(same_as<reference_t<std::array<int, 3>::iterator>, int&>);
 }
 
 TEST_CASE("iterators have a const reference type") {
-	static_assert(Same<const_reference_t<std::array<int, 3>::iterator>, const int&>);
+  static_assert(same_as<const_reference_t<std::array<int, 3>::iterator>, const int&>);
 }
+
